6192eead2b3f0828afefb015ccfb6e6426c8e8ef
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-08-18 21:01:54 -0500

    feat: Render the headers
    The headers indicate which column and row you are dealing with.
    
    The general format is taken from other spreadsheet programs.

--- main.go
+++ main.go
@@ -3,10 +3,14 @@ package main
 import (
 	"fmt"
 	"github.com/pkg/term"
+	"golang.org/x/crypto/ssh/terminal"
 	"os"
 )
 
+var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 var bytes []byte
+
+var frame = 0
 
 func alternateScreen() {
 	fmt.Printf("\033[?1049h")
@@ -14,6 +18,63 @@ func alternateScreen() {
 
 func normalScreen() {
 	fmt.Printf("\033[?1049l")
+}
+
+func screenDimensions() (int, int) {
+	width, height, err := terminal.GetSize(0)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error getting terminal size: %v\n", err)
+		return 0, 0
+	}
+	return width, height
+}
+
+func getColumnWidth(column int) int {
+	return 5
+}
+
+func getColumnName(column int) string {
+	column = column % len(alphabet)
+	return alphabet[column : column+1]
+}
+
+func fixedWidth(s string, width int) string {
+	return s
+}
+
+func renderHeadings(x int, y int) {
+	width, height := screenDimensions()
+
+	xoff := 3
+	for i := 0; xoff < width; i++ {
+		setCursorPosition(x+xoff, y)
+
+		columnWidth := getColumnWidth(i)
+		fmt.Printf("\033[7m")
+		fmt.Printf("%s", fixedWidth(getColumnName(i), columnWidth))
+		fmt.Printf("\033[0m")
+
+		xoff += columnWidth
+	}
+
+	for i := 0; i < height; i++ {
+		setCursorPosition(x, y+i+1)
+		fmt.Printf("\033[7m")
+		fmt.Printf("%s", fixedWidth(fmt.Sprintf("%d", i), 3))
+		fmt.Printf("\033[0m")
+	}
+}
+
+func render() {
+	setCursorPosition(1, 1)
+	fmt.Printf("Hello, World!\n")
+	frame++
+
+	renderHeadings(1, 3)
+}
+
+func setCursorPosition(x, y int) {
+	fmt.Printf("\033[%d;%df", y, x)
 }
 
 func makeCursorInvisible() {
@@ -42,8 +103,8 @@ func main() {
 
 	bytes = make([]byte, 3) // buffer to read escape sequences
 	for {
-		//		render()
-		//
+		render()
+
 		bytes[0] = 0
 		bytes[1] = 0
 		bytes[2] = 0
@@ -52,6 +113,7 @@ func main() {
 			fmt.Fprintf(os.Stderr, "Read error: %v\n", err)
 			break
 		}
+
 		if keyPressed(27, 0, 0, bytes) { // Escape
 			break
 		}
