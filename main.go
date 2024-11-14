d6d9a28f0b2adc580745641ab5b9dc5481e377d1
samchristywork <106397573+samchristywork@users.noreply.github.com>
2024-11-14 17:39:17 -0600

    refactor: Remove unused logging
    Previously, the code made a log file, but didn't write to it at all.

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"github.com/pkg/term"
-	"log"
 	"os"
 	"strconv"
 )
@@ -109,15 +108,6 @@ func handleKeypress(bytes []byte) {
 }
 
 func main() {
-	f, err := os.OpenFile("test.log", os.O_APPEND|os.O_CREATE|os.O_RDWR, 0666)
-	if err != nil {
-		fmt.Printf("error opening file: %v", err)
-	}
-
-	defer f.Close()
-
-	log.SetOutput(f)
-
 	if len(os.Args) != 2 {
 		fmt.Fprintf(os.Stderr, "Usage: %s <filename>\n", os.Args[0])
 		return
